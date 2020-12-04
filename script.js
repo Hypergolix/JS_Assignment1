928f9dbcc7de65068327fa3fb0275b2b1abecd9f
Hypergolix <66037340+Hypergolix@users.noreply.github.com>
2020-12-04 01:27:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let teamOGArr = [96, 108, 89];                   // Array for team OG
+let teamAArr = [88, 91, 110];                    // Array for team Alliance
+
+let total = [0, 0];                              // First stores teamOG average, second stores teamATotal average
+
+function calcAvg(teamArray) {
+    let avg = 0;
+    for (let i = 0; i < teamArray.length; i++) { // Or 3
+        avg = avg + teamArray[i];                // Value of each element is added together 
+    }
+    avg = avg / teamArray.length;                // Calculates the mean, total value divided by number of values
+    return avg;                                  // Returns the average
+}
+
+// Just prints averages for each team 
+total[0] = calcAvg(teamOGArr);                   // Get avg for team OG
+console.log("Team OG average: " + total[0]);
+
+total[1] = calcAvg(teamAArr);                    // Get avg for team Alliance
+console.log("Team Alliance average: " + total[1]);
+
+// This works but try other solutions below
+if (total[0] !== total[1]) {                     // If false, it's a tie 
+    if (total[0] < total[1]) {                   // If true, team Alliance wins
+        console.log("Team Alliance Wins!");
+    } else {
+        console.log("Team OG Wins!");
+    }
+} else {
+    console.log("It's a tie.");                  // Results in tie if the totals aren't equal
+}
+
+
+// Could we use concatenation so it's "Team " + ${name attributed to highest value variable} + " wins!" ? 
+// The names would have to be stored (plaintext is fine) and attributed to the highest score from total array. 
+// Switch might be useful here
+// Come back to this, no time
+
+
+// Switch? Case 1: is OG larger than A? Might be redundant.
+// Check if draw first when using if/else.
+// Might be over-engineering, check if totals really need to be stored.
+
+// Switch 
+// Case 1: 
